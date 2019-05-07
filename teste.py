7fea5a180548b062a9a9515ca378b15501f11393
Arthur Borges <abrothguer@gmail.com>
2019-05-07 19:01:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,29 @@
 
+"""
+    Remove repeating letters.
+"""
+# pylint: disable=C0103
+
+import re
+
+class RepeatReplacer(object):  # pylint: disable = R0903
+    """
+        Class implementation
+    """
+    def __init__(self):
+        self.repeat_regexp = re.compile(r'(\w*)(\w)\2(\w*)')
+        self.repl = r'\1\2\3'
+
+    def replace(self, word):
+        """
+            Replace repeating letters.
+        """
+        repl_word = self.repeat_regexp.sub(self.repl, word)
+
+        if repl_word != word:
+            return self.replace(repl_word)
+        return repl_word
+
+# Replacing
+replacer = RepeatReplacer()
+replacer.replace()
